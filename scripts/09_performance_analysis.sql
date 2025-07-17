d2e172c696f44480864892ff942908d2dc486b7f
Priyanka Mittal <139239417+priyamit08@users.noreply.github.com>
2025-07-17 16:35:46 +0200

    Create 09_performance_analysis.sql

---
+++ scripts/09_performance_analysis.sql
@@ -0,0 +1,18 @@
+/*
+===============================================================================
+Performance Analysis (Year-over-Year, Month-over-Month)
+===============================================================================
+Purpose:
+    - To measure the performance of products, customers, or regions over time.
+    - For benchmarking and identifying high-performing entities.
+    - To track yearly trends and growth.
+
+SQL Functions Used:
+    - LAG(): Accesses data from previous rows.
+    - AVG() OVER(): Computes average values within partitions.
+    - CASE: Defines conditional logic for trend analysis.
+===============================================================================
+*/
+
+/* Analyze the yearly performance of products by comparing their sales 
+to both the average sales performance of the product and the previous year's sales */
