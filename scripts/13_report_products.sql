5944014298eb614135bd14d25906f36e3ff46ccb
Priyanka Mittal <139239417+priyamit08@users.noreply.github.com>
2025-07-21 12:44:33 +0200

    Update 13_report_products.sql

--- scripts/13_report_products.sql
+++ scripts/13_report_products.sql
@@ -25,28 +25,6 @@ Highlights:
 -- =============================================================================
 
 
-/*
-===============================================================================
-Customer Report
-===============================================================================
-Purpose:
-    - This report consolidates key customer metrics and behaviors
-
-Highlights:
-    1. Gathers essential fields such as names, ages, and transaction details.
-	2. Segments customers into categories (VIP, Regular, New) and age groups.
-    3. Aggregates customer-level metrics:
-	   - total orders
-	   - total sales
-	   - total quantity purchased
-	   - total products
-	   - lifespan (in months)
-    4. Calculates valuable KPIs:
-	    - recency (months since last order)
-		- average order value
-		- average monthly spend */
-
-
 IF OBJECT_ID('gold.report_products', 'V') IS NOT NULL
     DROP VIEW gold.report_products;
 GO
